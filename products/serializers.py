a3ec3910a559767c39ffb1967ef58960188c9e56
Muhammad Azri Fatihah Susanto <Muhammad.azri.f.s@gmail.com>
2025-07-29 13:54:49 +0700

    feat: add product serializer with validation and hateoas support

---
+++ products/serializers.py
@@ -0,0 +1,120 @@
+from rest_framework import serializers
+from .models import Product
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    _links = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Product
+        fields = [
+            'id', 'name', 'sku', 'description', 'shop', 'location',
+            'price', 'discount', 'category', 'stock', 'is_available',
+            'picture', 'is_delete', '_links'
+        ]
+        read_only_fields = ['id', 'is_delete', '_links']
+
+    def get__links(self, obj):
+        request = self.context.get('request')
+        if not request:
+            base_url = 'http://localhost:8000'
+        else:
+            base_url = f"{request.scheme}://{request.get_host()}"
+        
+        return [
+            {
+                "rel": "self",
+                "href": f"{base_url}/products",
+                "action": "POST",
+                "types": ["application/json"]
+            },
+            {
+                "rel": "self",
+                "href": f"{base_url}/products/{obj.id}/",
+                "action": "GET",
+                "types": ["application/json"]
+            },
+            {
+                "rel": "self",
+                "href": f"{base_url}/products/{obj.id}/",
+                "action": "PUT",
+                "types": ["application/json"]
+            },
+            {
+                "rel": "self",
+                "href": f"{base_url}/products/{obj.id}/",
+                "action": "DELETE",
+                "types": ["application/json"]
+            }
+        ]
+
+    def validate_price(self, value):
+        if value < 0:
+            raise serializers.ValidationError("Price must be a positive integer.")
+        return value
+
+    def validate_discount(self, value):
+        if value < 0:
+            raise serializers.ValidationError("Discount must be a positive integer.")
+        return value
+
+    def validate_stock(self, value):
+        if value < 0:
+            raise serializers.ValidationError("Stock must be a positive integer.")
+        return value
+
+    def validate_sku(self, value):
+        if not value.strip():
+            raise serializers.ValidationError("SKU cannot be blank.")
+        return value
+
+    def validate_name(self, value):
+        if not value.strip():
+            raise serializers.ValidationError("Name cannot be blank.")
+        return value
+
+
+class ProductListSerializer(serializers.ModelSerializer):
+    _links = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Product
+        fields = [
+            'id', 'name', 'sku', 'description', 'shop', 'location',
+            'price', 'discount', 'category', 'stock', 'is_available',
+            'picture', 'is_delete', '_links'
+        ]
+
+    def get__links(self, obj):
+        request = self.context.get('request')
+        if not request:
+            base_url = 'http://localhost:8000'
+        else:
+            base_url = f"{request.scheme}://{request.get_host()}"
+        
+        return [
+            {
+                "rel": "self",
+                "href": f"{base_url}/products",
+                "action": "POST",
+                "types": ["application/json"]
+            },
+            {
+                "rel": "self",
+                "href": f"{base_url}/products/{obj.id}/",
+                "action": "GET",
+                "types": ["application/json"]
+            },
+            {
+                "rel": "self",
+                "href": f"{base_url}/products/{obj.id}/",
+                "action": "PUT",
+                "types": ["application/json"]
+            },
+            {
+                "rel": "self",
+                "href": f"{base_url}/products/{obj.id}/",
+                "action": "DELETE",
+                "types": ["application/json"]
+            }
+        ]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,56 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from django.db.models import Q
+from .models import Product
+from .serializers import ProductSerializer, ProductListSerializer
 
-# Create your views here.
+
+@api_view(['GET', 'POST'])
+def product_list(request):
+    if request.method == 'GET':
+        name_query = request.GET.get('name', '')
+        location_query = request.GET.get('location', '')
+        
+        products = Product.objects.filter(is_delete=False)
+        
+        if name_query:
+            products = products.filter(name__icontains=name_query)
+        
+        if location_query:
+            products = products.filter(location__icontains=location_query)
+        
+        serializer = ProductListSerializer(products, many=True, context={'request': request})
+        return Response({'products': serializer.data}, status=status.HTTP_200_OK)
+    
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data, context={'request': request})
+        if serializer.is_valid():
+            product = serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def product_detail(request, product_id):
+    try:
+        product = Product.objects.get(id=product_id, is_delete=False)
+    except Product.DoesNotExist:
+        return Response({'detail': 'Not found.'}, status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        serializer = ProductSerializer(product, context={'request': request})
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    elif request.method == 'PUT':
+        serializer = ProductSerializer(product, data=request.data, context={'request': request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'DELETE':
+        product.is_delete = True
+        product.save()
+        return Response(status=status.HTTP_204_NO_CONTENT)
